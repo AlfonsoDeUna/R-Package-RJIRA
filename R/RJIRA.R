9be09cae5980b969b2ebf62c79f711d8ccd1d428
briofons <briofons@gmail.com>
2015-12-09 01:07:15 +0100

    find issues from query

--- R/RJIRA.R
+++ R/RJIRA.R
@@ -12,10 +12,31 @@ simpleConnection <- function (URL, port, version=NULL) {
   return(simpleURL)
 }
 
+createRow <- function (dat) {
+  row <- c ( convertNull2NA(jsIssue$key),
+            convertNull2NA(jsIssue$fields$summary),
+            convertNull2NA(jsIssue$fields$issuetype$name),
+            convertNull2NA(jsIssue$fields$issuetype$id),
+            convertNull2NA(jsIssue$fields$status$name),
+            convertNull2NA(jsIssue$fields$status$id),
+            convertNull2NA(jsIssue$fields$reporter$name),
+            convertNull2NA(jsIssue$fields$reporter$emailAddress),
+            convertNull2NA(jsIssue$fields$reporter$displayName),
+            convertNull2NA(jsIssue$fields$assignee$name),
+            convertNull2NA(jsIssue$fields$assignee$emailAddress),
+            convertNull2NA(jsIssue$fields$assignee$displayName),
+            convertNull2NA(jsIssue$fields$project$id),
+            convertNull2NA(jsIssue$fields$project$name),
+            convertNull2NA(jsIssue$comment$total))
+  return(row)
+}
+
 # For retrieving the most interesting information from 
 # request issue to dataframe
 getIssue <- function (conn, key, type = "GET") {
-  jsIssue <- fromJSON(conn)
+  
+  con<-cat(conn,"/issue/",key,sep="")
+  jsIssue <- fromJSON(con)
   
   #create a table
   
@@ -57,6 +78,25 @@ dt <- data.frame ( convertNull2NA(jsIssue$key),
   
   return(dt)
                     
+}
+
+## for testing https://jira.atlassian.com/rest/api/latest/search?
+freeQuery <-function (conn, query = NULL) {
+  jsIssue <- fromJSON(conn)
+  lista<-jsIssue$issue$key
+  cont <-0
+  for (i in lista){
+    if (cont == 0){
+      data<-getIssue (conn, i)
+    }
+    else{
+      con<-cat(conn,"/issue/",key,sep="")
+      jsIssue <- fromJSON(con)
+      rbind (data, createRow(jsIssue))
+    }
+  }
+  
+  
 }
 
 convertNull2NA <- function (x) {
