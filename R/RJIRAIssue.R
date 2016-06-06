8253255a7c0062c6108a5de566f5535f8d696a44
briofons <briofons@gmail.com>
2016-06-07 01:00:13 +0200

    last modificattion first issue created feature

---
+++ R/RJIRAIssue.R
@@ -0,0 +1,31 @@
+##============================================================================
+## RJIRA - R interface with JIRA instance
+## CopyRight (c) 2016
+## @author Alfonso de Uña del Brio <briofons@gmail.com
+## 
+## Interface with JIRA with the idea
+## to get information that you need to start making
+## study about your issues.
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##=============================================================================
+
+createIssue<-function() {
+ 
+  # get metadata for creating issues
+  
+  # complete data to create
+  
+  # we need to use POST 
+}
